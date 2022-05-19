36890e8991bf657e0df977e7766b5340fa6eb46a
Navat2007 <navat2007@yandex.ru>
2022-05-19 21:43:06 +0300

    Update EnemyStandardController.cs

--- Assets/Scripts/Enemy/EnemyStandardController.cs
+++ Assets/Scripts/Enemy/EnemyStandardController.cs
@@ -4,7 +4,7 @@ public class EnemyStandardController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 0.25f;
     [SerializeField] private float stuckMaxTimer = 1f;
-    [SerializeField] private float stuckMaxCount = 5f;
+    [SerializeField] private float stuckMaxCount = 6f;
     
     private EnemyController _enemyController;
     private HealthSystem _healthSystem;
