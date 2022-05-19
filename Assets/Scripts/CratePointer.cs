037e895f4f516d127bb2844ffc33c70aaa760022
Navat2007 <navat2007@yandex.ru>
2022-05-19 22:54:55 +0300

    Обновил толстого

--- Assets/Scripts/CratePointer.cs
+++ Assets/Scripts/CratePointer.cs
@@ -11,24 +11,27 @@ public class CratePointer : MonoBehaviour
 
     private void Update()
     {
-        Vector3 fromPlayerToEnemy = transform.position - ControllerManager.player.transform.position;
-        Ray ray = new Ray(ControllerManager.player.transform.position, fromPlayerToEnemy);
-        //Debug.DrawRay(ControllerManager.player.transform.position, fromPlayerToEnemy);
-
-        // Ordering: [0] = Left, [1] = Right, [2] = Down, [3] = Up, [4] = Near, [5] = Far
-        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(_camera);
-
-        float minDistance = Mathf.Infinity;
-        
-        for (int i = 0; i < 4; i++)
+        if (ControllerManager.player != null)
         {
-            if (planes[i].Raycast(ray, out float distance))
-            {
-                if (distance < minDistance)
-                    minDistance = distance;
-            }
-        }
+            Vector3 fromPlayerToEnemy = transform.position - ControllerManager.player.transform.position;
+            Ray ray = new Ray(ControllerManager.player.transform.position, fromPlayerToEnemy);
+            //Debug.DrawRay(ControllerManager.player.transform.position, fromPlayerToEnemy);
 
-        Vector3 worldPosition = ray.GetPoint(minDistance);
+            // Ordering: [0] = Left, [1] = Right, [2] = Down, [3] = Up, [4] = Near, [5] = Far
+            Plane[] planes = GeometryUtility.CalculateFrustumPlanes(_camera);
+
+            float minDistance = Mathf.Infinity;
+        
+            for (int i = 0; i < 4; i++)
+            {
+                if (planes[i].Raycast(ray, out float distance))
+                {
+                    if (distance < minDistance)
+                        minDistance = distance;
+                }
+            }
+
+            Vector3 worldPosition = ray.GetPoint(minDistance);
+        }
     }
 }
